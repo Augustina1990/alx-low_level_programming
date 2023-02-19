4239655ac2489a4c15a02ba4bbc88a67cdab7126
Augustina1990 <igbokwemy@gmail.com>
2023-02-19 12:15:04 -0800

    Hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+ 
+ /**
+  * main -print the phrase programming is like building a multilingual puzzle
+  * return: 0 if exited properly, not zer0 otherwise
+  */
+ int main (void)
+  {
+	  put ("\"programming is like buiding a multilingual puzzle");
+	  return (0);
+  }
