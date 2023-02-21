1f17de17a620f0ddb9f2191042e23c2a36ad6083
Augustina1990 <igbokwemy@gmail.com>
2023-02-20 20:58:47 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - assigns a random number to variable in e   ach time
- * it executes an prints it
+ * it executes and  prints it
  * Return: Always 0 (success)
  */
+
 int main(void)
+
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
         if(n > 0)
-		printf("%d is positive\n", n);
+	    	printf("%d is positive\n", n);
+
         else if (n < 0)
 		printf("%d is negative\n", n);
+
 	else if (n == 0)
 		printf("%d is zero\n", n);
+
 	return (0);
+
 }
