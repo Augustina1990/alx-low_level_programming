62630217ff7541f44a3ffa5bf50143836b78f2c5
Augustina1990 <igbokwemy@gmail.com>
2023-02-27 05:34:50 -0800

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,13 @@
  /**
   *_strlen - checks the length of a string
   *@s: input
+  *
   *Return: Always 0 (success)
   */
 int _strlen(char *s)
 {
 	int lenght;
 
-	lenght = _strlen(s);
+	lenght = strlen(s);
 	return (lenght);
 }
